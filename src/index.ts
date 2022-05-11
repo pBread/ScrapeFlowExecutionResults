1547dc44c6e574c3d4d3af6873eb8eda819fd0f4
Phil <pbrede21@gmail.com>
2022-05-11 17:52:44 -0600

    working

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,33 @@
 import dotenv from "dotenv";
 import twilio from "twilio";
+import _ from "lodash";
 dotenv.config();
+
+const FLOW_SID = "FW8abdd988a49910442e03eca1d61d5a38";
 
 const { ACCOUNT_SID, AUTH_TOKEN } = process.env;
 const client = twilio(ACCOUNT_SID, AUTH_TOKEN);
 
-const FLOW_SID = "FW8abdd988a49910442e03eca1d61d5a38";
-
-const counter = { "1": 0, "2": 0 };
+const counter = {}; // counts the number of times each option was selected
+const WIDGET_NAME = "MyGatherWidget"; // the name of your Studio Flow's gather widget you are analyzing
 
 (async () => {
   const executionSids = await client.studio
     .flows(FLOW_SID)
-    .executions.list()
-    .then((executions) => executions.map(({ sid }) => sid));
+    .executions.list() // fetch all execution of this Studio Flow
+    .then((executions) => executions.map(({ sid }) => sid)); // we only need the sid of the execution
 
   for (const executionSid of executionSids) {
-    const executionContext = await client.studio
+    const widgets = await client.studio
       .flows(FLOW_SID)
       .executions(executionSid)
       .executionContext(executionSid)
-      .fetch();
+      .fetch() // fetch the executionContext record
+      .then((executionContext) => executionContext.context.widgets); // the .context.widgets property contains all of the input results of this execution
 
-    counter[executionContext.context.widgets.MyGatherWidget?.Digits]++;
+    if (_.has(counter, widgets[WIDGET_NAME]?.Digits))
+      counter[widgets[WIDGET_NAME]?.Digits]++;
+    else counter[widgets[WIDGET_NAME]?.Digits] = 1;
   }
 
   console.log(counter);
