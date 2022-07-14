a2674b3fb7d0a2892561f29413be4fec5c46b6e8
Phil <pbrede21@gmail.com>
2022-07-14 10:12:18 -0600

    redesigned to

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,63 @@
 import dotenv from "dotenv";
 import twilio from "twilio";
 import _ from "lodash";
-dotenv.config();
 
-const FLOW_SID = "FW8abdd988a49910442e03eca1d61d5a38";
+dotenv.config();
 
 const { ACCOUNT_SID, AUTH_TOKEN } = process.env;
 const client = twilio(ACCOUNT_SID, AUTH_TOKEN);
 
-const counter = {}; // counts the number of times each option was selected
-const WIDGET_NAME = "MyGatherWidget"; // the name of your Studio Flow's gather widget you are analyzing
-
 (async () => {
+  const counters = {};
+
+  const flows = await client.studio.flows
+    .list()
+    .then((flows) =>
+      flows.filter((flow) => flow.sid === "FW8abdd988a49910442e03eca1d61d5a38")
+    );
+
+  for (const flow of flows) {
+    const results = await aggregateFlowResults(flow.sid, flow.friendlyName);
+    counters[flow.friendlyName] = results;
+  }
+
+  console.log(counters);
+})();
+
+async function aggregateFlowResults(flowSid: string, flowName: string) {
+  const counter = {};
+
   const executionSids = await client.studio
-    .flows(FLOW_SID)
+    .flows(flowSid)
     .executions.list() // fetch all execution of this Studio Flow
     .then((executions) => executions.map(({ sid }) => sid)); // we only need the sid of the execution
 
   for (const executionSid of executionSids) {
-    const widgets = await client.studio
-      .flows(FLOW_SID)
-      .executions(executionSid)
-      .executionContext(executionSid)
-      .fetch() // fetch the executionContext record
-      .then((executionContext) => executionContext.context.widgets); // the .context.widgets property contains all of the input results of this execution
+    try {
+      const result = await getExecutionResult(flowSid, executionSid);
 
-    if (_.has(counter, widgets[WIDGET_NAME]?.Digits))
-      counter[widgets[WIDGET_NAME]?.Digits]++;
-    else counter[widgets[WIDGET_NAME]?.Digits] = 1;
+      if (_.has(counter, result)) counter[result]++;
+      else counter[result] = 1;
+    } catch (error) {}
   }
 
-  console.log(counter);
-})();
+  return Object.assign(counter, {
+    _total: _.sum(Object.values(counter)),
+    flowName,
+  });
+}
+
+async function getExecutionResult(flowSid: string, executionSid: string) {
+  const widgets = await client.studio
+    .flows(flowSid)
+    .executions(executionSid)
+    .executionContext(executionSid)
+    .fetch()
+    .then(({ context }) => context.widgets);
+
+  const gatherWidget: { Digits?: string } = Object.values(widgets).find(
+    (widget) => _.has(widget, "Digits")
+  );
+
+  return gatherWidget?.Digits || null;
+}
